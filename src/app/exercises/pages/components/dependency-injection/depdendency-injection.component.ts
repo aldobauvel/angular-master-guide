c5f1e5b8cee1e38df535dad09c3069e952d01f28
aldo <aldobauvel@gmail.com>
2025-03-29 10:19:05 -0600

    dependency injection

--- src/app/exercises/pages/components/dependency-injection/depdendency-injection.component.ts
+++ src/app/exercises/pages/components/dependency-injection/depdendency-injection.component.ts
@@ -10,25 +10,52 @@ enum CarType {
   audi
 }
 
-
 @Component({
   selector: 'app-depdendency-injection',
   templateUrl: './depdendency-injection.component.html',
-  styleUrls: ['./depdendency-injection.component.css']
+  styleUrls: ['./depdendency-injection.component.css'],  
 })
 export class DepdendencyInjectionComponent implements OnInit, AfterViewInit {
 
-  code: string = `function hello() {\n  console.log("Hola, Monaco Editor!");\n}`;
-  editorOptions = { theme: 'vs-dark', language: 'javascript' };
+  constructor(private testService: TestsService, private monacoLoader: MonacoLoaderService) {}
+
+  code: string = `constructor(private testService: TestsService, private monacoLoader: MonacoLoaderService) {}`;
+  editorOptions = { theme: 'vs-dark', language: 'typescript' };
   monacoLoaded = false;
 
+  serviceCode: string = `
+  @Injectable({
+    providedIn: 'root'//Especifica que el servicio debe proporcionarse en el inyector raíz (AppModule) o sea disponible en toda la aplicación
+  })`;
+
+  serviceCodeSpecify: string = ` import { Injectable } from '@angular/core';
+ import { UserModule } from './user.module'; // modulo específico
+  @Injectable({
+    providedIn:  UserModule, // modulo específico
+  })
+  export class UserService {
+  }`;
   
+  //Set service in a module
+  serviceInModule : string = ` import { NgModule } from '@angular/core';';
+ import { UserService } from './user.service';
+@NgModule({
+  providers: [UserService],  //Servicio a inyectar a nivel modulo
+})
+  export class UserModule  {
+  }`;
 
-  users: any = [];
+  //Set DI in component
+  serviceInComponent : string = `@Component({
+/* . . . */
+  providers: [UserService]
+})
+.
+.
+.
+constructor(private testService: TestsService, private monacoLoader: MonacoLoaderService) {}`;
 
-  constructor(private testService: TestsService, private monacoLoader: MonacoLoaderService) {
-    console.log(CarType.volvo);
-  }
+  users: any = [];  
 
   ngAfterViewInit() {
     this.monacoLoader.loadMonaco().then(() => {
